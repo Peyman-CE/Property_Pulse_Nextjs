99bdca2097c3d24d936f5067d63a52299e4182b5
peyman <amirmahani.ce@gmail.com>
2026-08-02 13:15:26 +0330

    added two new state for drop down button menus (one for hamburger and one for profile)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,16 @@
+'use client'
+import {useState} from "react"
 import Link from "next/link";
 import logo from "@/assets/images/logo-white.png"
 import Image from "next/image";
 import profileDefault from "@/assets/images/profile.png"
+import { FaGoogle } from "react-icons/fa"
 
 const Navbar = () => {
+    const [isMobileMenuopen ,setIsMobilemenuOpen] = useState(false);
+    const [isProfileMenuOpen ,setIsProfileMenuOpen] = useState(false);
+
+
     return ( 
         <nav className="bg-blue-700 border-b border-blue-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -16,6 +23,7 @@ const Navbar = () => {
                             className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                             aria-controls="mobile-menu"
                             aria-expanded="false"
+                            onClick={() => setIsMobilemenuOpen((prev) => !prev)}
                         >
                             <span className="absolute -inset-0.5"></span>
                             <span className="sr-only">Open main menu</span>
@@ -81,7 +89,7 @@ const Navbar = () => {
                                 href="/auth/login"
                                 className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                             >
-                                <i className="fa-brands fa-google text-white mr-2"></i>
+                                <FaGoogle className="text-white mr-2"/>
                                 <span>Login or Register</span>
                             </Link>
                         </div>
@@ -125,6 +133,7 @@ const Navbar = () => {
                                     id="user-menu-button"
                                     aria-expanded="false"
                                     aria-haspopup="true"
+                                    onClick={() => setIsProfileMenuOpen((prev) => !prev)}
                                 >
                                     <span className="absolute -inset-1.5"></span>
                                     <span className="sr-only">Open user menu</span>
@@ -137,9 +146,10 @@ const Navbar = () => {
                             </div>
 
                             {/* Profile dropdown */}
+                            {isProfileMenuOpen && (
                             <div
                                 id="user-menu"
-                                className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 role="menu"
                                 aria-orientation="vertical"
                                 aria-labelledby="user-menu-button"
@@ -172,13 +182,16 @@ const Navbar = () => {
                                     Sign Out
                                 </button>
                             </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
+            
 
             {/* Mobile menu */}
-            <div className="hidden" id="mobile-menu">
+            {isMobileMenuopen && (
+            <div id="mobile-menu">
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     <Link
                         href="/"
@@ -207,6 +220,7 @@ const Navbar = () => {
                     </Link>
                 </div>
             </div>
+            )}
         </nav>
     );
 }
