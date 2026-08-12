4feec620c3affb578d1ac8813f64d4cab4dc6064
peyman <amirmahani.ce@gmail.com>
2026-08-12 11:53:41 +0330

    created edit page for each product and added a new component for prperty edit

--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { toast } from "react-toastify";
 import deleteProperty from "@/app/actions/deleteProperty";
 
 const ProfileProperties = ({ properties: initialProperties }) => {
@@ -16,6 +17,7 @@ const ProfileProperties = ({ properties: initialProperties }) => {
 
     const updatedProperties = properties.filter((property) => property._id !== propertyId);
     setProperties(updatedProperties);
+    toast.success("Property deleted successfully");
   }
 
   return properties.map((property) => (
@@ -37,12 +39,12 @@ const ProfileProperties = ({ properties: initialProperties }) => {
         </p>
       </div>
       <div className="mt-2">
-        <a
-          href="/add-property.html"
+        <Link
+          href={`/properties/${property._id}/edit`}
           className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
         >
           Edit
-        </a>
+        </Link>
         <button
           className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
           type="button"
