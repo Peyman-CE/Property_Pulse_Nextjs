46adf44ce1a69c05d02bc213cd8d39c1ae4c5b59
peyman <amirmahani.ce@gmail.com>
2026-08-15 12:06:38 +0330

    added spinner for map

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -6,8 +6,8 @@ const override = {
     margin: "100px auto",
 };
 
-const LoadingPage = () => {
+const Spinner = () => {
     return ( <ClipLoader color="#3b82f6" cssOverride={override} size={150} aria-label="Loading Spinner" /> );
 }
  
-export default LoadingPage;
+export default Spinner;
